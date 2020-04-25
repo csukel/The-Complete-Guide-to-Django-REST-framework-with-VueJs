eeb6e553a8489ff1180ff61559257abdc8101a68
Loucas Stylianou <loukkis91@gmail.com>
2020-04-25 17:59:03 +0300

    Added swagger app for browsing the apis

--- 04-DRF-LEVEL-TWO/ebooksapi/ebooks/api/urls.py
+++ 04-DRF-LEVEL-TWO/ebooksapi/ebooks/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from ebooks.api.views import EbookListCreateAPIView, EbookDetailAPIView, ReviewCreateAPIView, ReviewDetailAPIView
 
 urlpatterns = [
--- 04-DRF-LEVEL-TWO/ebooksapi/ebooksapi/settings.py
+++ 04-DRF-LEVEL-TWO/ebooksapi/ebooksapi/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'ebooks'
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
